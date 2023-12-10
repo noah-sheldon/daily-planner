58794859e935b9ff7c0510eb3566227901df886c
Noah Sheldon <noahsheldon06@gmail.com>
2023-12-10 23:47:13 +0000

    Fixed colours, cleaned code, added comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,33 +4,13 @@ let tasksMap = getTasks();
 
 $("#currentDay").text(today);
 
-for (let i = 9; i < 24; i++) {
-  // Current hour
-  let hour = dayjs().hour(i).format("hA");
+startApp();
 
-  let row = $("<div>").addClass("row time-block");
-  container.append(row);
-
-  let hourDiv = $("<div>").addClass("col-md-2 hour").text(hour);
-
-  let textArea = $("<textarea>").addClass("col-md-9 description").attr("id", i);
-  if (tasksMap[hour]) {
-    textArea.val(tasksMap[hour]);
-  }
-
-  let saveBtn = $("<button>").addClass("col-md-1 saveBtn").text("Save");
-
-  row.append(hourDiv, textArea, saveBtn, $('<br>'));
-  
-  if (+dayjs().hour(i).format("H") < dayjs().hour()) {
-    $("#"+i).css("background", "red");
-  } else if (+dayjs().hour(i).format("H") === dayjs().hour()) {
-    $("#"+i).css("background", "grey");
-  } else {
-    $("#"+i).css("background", "green");
-  }
+function startApp() {
+  createHourBlocks();
 }
 
+// SaveBtn click event
 $(".saveBtn").on("click", function (event) {
   event.preventDefault();
   let task = $(this).siblings(".description").val();
@@ -47,10 +27,63 @@ $(".saveBtn").on("click", function (event) {
   }
 });
 
+//Get tasksMap from localStorage
 function getTasks() {
   if ("tasksMap" in localStorage) {
     return JSON.parse(localStorage.getItem("tasksMap"));
   } else {
     return {};
+  }
+}
+
+function createHourBlocks() {
+  for (let i = 9; i < 18; i++) {
+    // Current hour in format: 9AM
+    let hour = dayjs().hour(i).format("hA");
+
+    //Create row for every hour
+    let row = $("<div>").addClass("row time-block");
+    container.append(row);
+
+    //Create div for hour
+    let hourDiv = $("<div>")
+      .addClass(
+        "col-md-2 hour text-center fw-bold d-flex align-items-center justify-content-center"
+      )
+      .text(hour);
+
+    //Create textArea for every hour
+    let textArea = $("<textarea>")
+      .addClass("col-md-8 description")
+      .attr("id", i);
+
+    //If task present, append value to textArea
+    if (tasksMap[hour]) {
+      textArea.val(tasksMap[hour]);
+    }
+
+    //Create saveButton
+    let saveBtn = $("<button>")
+      .addClass(
+        "col-md-2 saveBtn fw-bold text-center d-flex align-items-center justify-content-center"
+      )
+      .text("Save");
+
+    // Append hour, textArea, and SaveBtn to row parent
+    row.append(hourDiv, textArea, saveBtn, $("<br>"));
+
+    //Apply background conditionally
+    applyBackground(i);
+  }
+}
+
+function applyBackground(hour) {
+  //Background colour by hour logic
+  if (+dayjs().hour(hour).format("H") < dayjs().hour()) {
+    $("#" + hour).css("background", "#ff9999");
+  } else if (+dayjs().hour(hour).format("H") === dayjs().hour()) {
+    $("#" + hour).css("background", "#c2c2d6");
+  } else {
+    $("#" + hour).css("background", "#b3ffe0");
   }
 }
